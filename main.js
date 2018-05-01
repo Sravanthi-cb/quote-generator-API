7ff7c99605e45ffc19712d277c4d4ace4fef726b
Sravanthi <sravanthi.cb@gmail.com>
2018-05-01 16:24:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   //Function for quote api
   function getNewQuote(){  
 	$.ajax({
-	  url: 'https://cors-anywhere.herokuapp.com/http://quotes.stormconsultancy.co.uk/random.json',
+	  url: 'http://quotes.stormconsultancy.co.uk/random.json',
       success: function(response) {
 	     quote =  response.quote;
 		 author = response.author;
